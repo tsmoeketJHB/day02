11e17e38903b20ee98b07fcfb0e7f86fa96ba9c2
tsmoeketJHB <tsmoeket@c4r14s5.wethinkcode.co.za>
2020-07-10 17:42:48 +0200

    exercises

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void ft_putchar(char c )
+{
+	write( 1, &c, 1);
+}
+void ft_print_alphabet(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet >= 'a' && alphabet <= 'z' )
+	{
+       ft_putchar(alphabet);
+        alphabet++;
+  }
+}
+int main(void)
+{ 
+	ft_print_alphabet();
+	return(0);
+}
---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void ft_print_alphabet(void)
+{ 
+	char alphabet;
+
+	alphabet = 'z';
+
+	while (alphabet<='z' && alphabet >='a')
+	{
+		ft_putchar(alphabet);
+		alphabet--;
+	}
+}
+int main()
+{
+
+	ft_print_alphabet();
+	return(0);
+}
+
